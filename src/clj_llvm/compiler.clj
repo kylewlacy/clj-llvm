7ace9294d59e7547afd39310a7d83a626d063cc3
Kyle Lacy <kylewlacy@me.com>
2014-07-01 18:43:06 -0700

    Add rudimentary LLVM compilation

--- src/clj_llvm/compiler.clj
+++ src/clj_llvm/compiler.clj
@@ -1,7 +1,101 @@
 (ns clj-llvm.compiler
-  (:require [clojure.pprint    :refer [pprint]]
-            [clojure.java.io   :refer [reader]]
-            [clj-llvm.analyzer :as    analyzer]))
+  (:require [clojure.pprint       :refer [pprint]]
+            [clojure.java.io      :refer [reader]]
+            [mjolnir.constructors-init]
+            [mjolnir.core         :as    mjolnir]
+            [mjolnir.llvm-builder :as    builder]
+            [mjolnir.types        :as    types]
+            [mjolnir.expressions  :as    expr]
+            [mjolnir.config       :as    config]
+            [clj-llvm.analyzer    :as    analyzer])
+  (:alias c mjolnir.constructors))
 
-(defn -main [filename & args]
-  (pprint (analyzer/analyze-file filename (analyzer/empty-env))))
+(def ^:dynamic *globals*)
+
+
+
+(defmulti gen-expr :op)
+
+(defmethod gen-expr :do [{:keys [statements ret]}]
+  (let [statements (if statements (concat statements [ret])
+                                  [ret])]
+    (apply c/do (map gen-expr statements))))
+
+(defmethod gen-expr :const [{:keys [val]}]
+  (c/const val -> types/Int64))
+
+(defmethod gen-expr :invoke [{*fn :fn args :args}]
+  (expr/->Call (gen-expr *fn) (mapv gen-expr args)))
+
+; TODO: Multiple methods
+(defmethod gen-expr :fn [{:keys [methods]}]
+  (gen-expr (first methods)))
+
+(defmethod gen-expr :fn-method [{:keys [params body]}]
+    (expr/map->Fn
+      {:type      (c/fn-t (repeat (count params) types/Int64)
+                          types/Int64)
+       :name      (str (gensym "fn"))
+       :arg-names (mapv :name params)
+       :body      (gen-expr body)}))
+
+; TODO: Locals other than args
+(defmethod gen-expr :local [{:keys [arg-id]}]
+  (expr/->Arg arg-id))
+
+(defmethod gen-expr :def [{{name :name ns* :ns} :var init :init}]
+  (let [init (gen-expr init)]
+    (swap! *globals* assoc-in [ns* name] init)
+    (if (types/FunctionType? (:type init))
+        init
+        (c/global (str ns* "/" name) (:type init) init))))
+
+(defmethod gen-expr :var [{{name :name ns* :ns} :var}]
+  (expr/->Gbl (str ns* "/" name)))
+
+; TODO: Pass meta to LLVM (somehow?)
+(defmethod gen-expr :with-meta [{:keys [expr]}]
+  (gen-expr expr))
+
+(defmethod gen-expr :default [ast]
+  (println "Don't know how to gen AST:" ast))
+
+
+
+(defn gen-module [main-ns & exprs]
+  (apply c/module [] (concat exprs
+    [(c/fn "main" (c/fn-t [] types/Int64) [] :extern
+      (expr/->Call
+        (expr/->Gbl (:name (get-in @*globals*
+                                   [main-ns '-main])))
+        []))])))
+
+; TODO: Garbage collection (link Boehm GC)
+(defn build-module [main-ns & asts]
+  (with-bindings {#'*globals* (atom {})
+                  #'config/*int-type* types/Int64
+                  #'config/*float-type* types/Float64
+                  #'config/*gc* nil
+                  #'config/*target* (config/default-target)}
+    (-> (apply gen-module main-ns (map gen-expr asts))
+        mjolnir/to-db
+        mjolnir/to-llvm-module)))
+
+(defn -main [input-file main-ns output-exe & args]
+  (mjolnir/to-exe
+    (apply build-module
+      (symbol main-ns)
+      (analyzer/analyze-file input-file
+                             (analyzer/empty-env)))
+    output-exe))
+
+
+
+(comment
+  ; Sample file emission
+  (mjolnir.targets.target/emit-to-file
+    (mjolnir.config/default-target)
+    (:module (build-module 'user (analyzer/analyze
+                '(def -main (fn* -main [] 2)))))
+    {:output-type :asm})
+)
